860d6609dbf2c56bef03129f9f68ef587146fce8
quanghai29 <lequanghai540@gmail.com>
2021-06-11 21:20:21 +0700

    add test suite

---
+++ Test Suites/DemoTestSuite.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DemoTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cc1f8c73-c21c-4c4b-b14b-0fdb961066c8</testSuiteGuid>
+   <testCaseLink>
+      <guid>edb43e4c-3cd2-4635-8650-c2902fce162a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Test_1_GetAllCategories</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>33ab400e-880c-4c5d-b11a-b8d1f4013bf6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Test_2_GetCourseDetail</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ea4579fb-5012-48fa-92ec-8a9a7cff3cd8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Test_3_GetAllCourse</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
