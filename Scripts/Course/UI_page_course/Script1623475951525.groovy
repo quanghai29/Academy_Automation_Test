b93ed1c6f0571f38c5ae87aa4fd78c8d475a47ce
LinhDong64 <mailinhdong@gmail.com>
2021-06-12 17:42:33 +0700

    update test case

--- Scripts/Course/UI_page_course/Script1623475951525.groovy
+++ Scripts/Course/UI_page_course/Script1623475951525.groovy
@@ -19,6 +19,7 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
+
 WebUI.navigateToUrl('https://academy-for-test.herokuapp.com/view/course')
 
 WebUI.click(findTestObject('Object Repository/Page_Course/button_Load course'))
